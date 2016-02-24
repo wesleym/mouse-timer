b4a266cc143ba53a81fa3e4ef4be534214f98d39
Wesley Moy <wesley@wesley-nextdoor.local>
2016-02-24 15:10:08 -0800

    Hook up the input

--- main.js
+++ main.js
@@ -9,22 +9,46 @@ function refresh() {
   flooringFraction = 1000
   itemFraction = 60
   var seconds = Math.floor(remainingMillis / flooringFraction) % itemFraction
-  if (seconds < 10) {
-    seconds = '0' + seconds
-  }
   flooringFraction *= itemFraction
   itemFraction = 60
   var minutes = Math.floor(remainingMillis / flooringFraction) % itemFraction
-  if (minutes < 10) {
-    minutes = '0' + minutes
-  }
   flooringFraction *= itemFraction
   itemFraction = 24
   var hours = Math.floor(remainingMillis / flooringFraction) % itemFraction
   flooringFraction *= itemFraction
   var days = Math.floor(remainingMillis / flooringFraction)
-  timer.innerText = days + ' ' + hours + ':' + minutes + ':' + seconds
+  timer.innerText = days + ' ' + hours + ':' + pad(minutes, 2) + ':' + pad(seconds, 2)
 }
 
+function destinationUpdated() {
+  destinationDate = new Date(input.value)
+  localStorage.setItem('destinationDate', destinationDate.toISOString())
+  refresh()
+}
+
+function init() {
+  if (localStorage.destinationDate) {
+    var isoString = localStorage.destinationDate
+    destinationDate = new Date(isoString)
+    console.log(createLocalDatetime(destinationDate))
+    input.value = createLocalDatetime(destinationDate)
+  }
+}
+
+function createLocalDatetime(date) {
+  return date.getFullYear() + '-' + pad(date.getMonth() + 1, 2) + '-' + pad(date.getDate(), 2) + 'T' + pad(date.getHours(), 2) + ':' + pad(date.getMinutes(), 2) + ':' + pad(date.getSeconds(), 2) + '.' + date.getMilliseconds()
+}
+
+function pad(a, digits) {
+  var result = '' + a
+  if (result.length < digits) {
+    return '0'.repeat(digits - result.length) + result;
+  } else {
+    return result
+  }
+}
+
+init()
 refresh()
 setInterval(refresh, 500)
+input.addEventListener('input', destinationUpdated)
